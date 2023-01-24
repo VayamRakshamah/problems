912d9a1e4df4897a66e94e548babed4710f06452
Vinay Gupta <vinaygupta890@gmail.com>
2023-01-24 08:04:57 +0530

    Alternating subarray

--- src/com/practice/intermediate/arrays/subarray/AlternatingSubarray.java
+++ src/com/practice/intermediate/arrays/subarray/AlternatingSubarray.java
@@ -1,5 +1,13 @@
 package com.practice.intermediate.arrays.subarray;
 
+//You are given an integer array A of length N comprising of 0's & 1's, and an integer B.
+//
+//You have to tell all the indices of array A that can act as a center of 2 * B + 1 length 0-1 alternating subarray.
+//
+//A 0-1 alternating array is an array containing only 0's & 1's,
+// and having no adjacent 0's or 1's. For e.g. arrays [0, 1, 0, 1], [1, 0] and [1] are 0-1 alternating,
+// while [1, 1] and [0, 1, 0, 0, 1] are not.
+
 import java.util.ArrayList;
 
 public class AlternatingSubarray {
