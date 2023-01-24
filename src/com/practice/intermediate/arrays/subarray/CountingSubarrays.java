9b57c9b5ba45a4c344e8f04204d2fdba8482f0f1
Vinay Gupta <vinaygupta890@gmail.com>
2023-01-24 08:10:27 +0530

    counting subarray and good subarray

--- src/com/practice/intermediate/arrays/subarray/CountingSubarrays.java
+++ src/com/practice/intermediate/arrays/subarray/CountingSubarrays.java
@@ -1,5 +1,9 @@
 package com.practice.intermediate.arrays.subarray;
 
+//Given an array A of N non-negative numbers and a non-negative number B,
+//you need to find the number of subarrays in A with a sum less than B.
+//We may assume that there is no overflow.
+
 import java.util.ArrayList;
 
 public class CountingSubarrays {
--- src/com/practice/intermediate/arrays/subarray/GoodSubarrays.java
+++ src/com/practice/intermediate/arrays/subarray/GoodSubarrays.java
@@ -1,5 +1,10 @@
 package com.practice.intermediate.arrays.subarray;
 
+//Given an array of integers A, a subarray of an array is said to be good if it fulfills any one of the criteria:
+//1. Length of the subarray is be even, and the sum of all the elements of the subarray must be less than B.
+//2. Length of the subarray is be odd, and the sum of all the elements of the subarray must be greater than B.
+//Your task is to find the count of good subarrays in A.
+
 import java.util.ArrayList;
 
 public class GoodSubarrays {
